5c0a50a0ebd53af43a8d31d5c91d39df72c4739a
lightyears1998 <lightyears1998@hotmail.com>
2018-01-09 13:15:56 +0800

    B题的构造解法

---
+++ winter/epsilon/b.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// LeaderBoard
+// https://vjudge.net/solution/5440093
+// “往杯子里面装水”
+
+int main()
+{
+    int m, s; cin >> m >> s;
+
+    if (m == 0 || s == 0 && m > 1 || s > m * 9) cout << "-1 -1" << endl;
+    else
+    {
+        for (int i = m, j = s; i; i--)
+        {
+            int k = max(0, j - 9 * (i - 1));
+            if (i == m && k == 0 && j) k = 1;
+            cout << k, j -= k;
+        }
+        cout << " ";
+        for (int i = m, j = s; i; i--)
+        {
+            int k = min(9, j);
+            cout << k, j -= k;
+        }
+        cout << endl;
+    }
+}
